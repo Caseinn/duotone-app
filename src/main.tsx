d7645cdd37b2f703abd3e8ca0fd4ed8694ed38fd
Caseinn <ditorifki.irawan@gmail.com>
2026-01-05 03:26:15 +0700

    chore: update main entry

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const allowedPaths = new Set(['/', '/index.html'])
+
+if (!allowedPaths.has(window.location.pathname)) {
+  window.location.replace('/404.html')
+} else {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
